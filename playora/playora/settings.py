61e5c2a2c0f7f906512665b277fe6f6933759278
zaid-vhora <mozaidv@gmail.com>
2026-07-21 16:34:00 +0530

    sss

--- playora/playora/settings.py
+++ playora/playora/settings.py
@@ -146,6 +146,6 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CART_SESSION_ID = 'cart'
