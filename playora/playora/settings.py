3a678992c9360dc8ed547244d096cb21acc0f809
zaid-vhora <mozaidv@gmail.com>
2026-07-21 15:55:58 +0530

    Added STATIC_ROOT for Render

--- playora/playora/settings.py
+++ playora/playora/settings.py
@@ -139,6 +139,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = 'static/'
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CART_SESSION_ID = 'cart'
